fbaca3c900bbc78b8c6e4a69b3d7febcb43738c1
Yakari2268 <reqential@gmail.com>
2025-06-25 07:14:06 +0530

    updated files

--- fde_sol/chaoticSystemSol.m
+++ fde_sol/chaoticSystemSol.m
@@ -5,8 +5,8 @@ clear; clc; close all;
 % --- 1. System Selection ---
 %system_choice = 'Lorenz';
 %system_choice = 'Chen';
-%system_choice = 'Chua';
- system_choice = 'Bhalekar-Geiji';
+system_choice = 'Chua';
+%system_choice = 'Bhalekar-Gejji';
 
 fprintf('Selected system: %s\n', system_choice);
 
@@ -33,21 +33,22 @@ switch system_choice
                          y(1) * y(2) - p.b * y(3) ];
         
         plot_title = 'Fractional-Order Chen System';
-
+    
+    %Remark: Does not work for chua, incorrect plot
     case 'Chua'
-        p.p = 10; p.q = 14.87; p.r = 18; p.m0 = -1.143; p.m1 = -0.714;
-        chua_diode = @(x, m0, m1) m1*x + 0.5*(m0 - m1)*(abs(x+1) - abs(x-1));
-        y0 = [0.1, 0.2, -0.1];
+        p.alpha = 15.6; p.beta = 28; p.m0 = -8/7; p.m1 = -5/7;
+        h = @(x, m0, m1) m1*x + 0.5*(m0 - m1)*(abs(x+1) - abs(x-1));
+        y0 = [0.1, 0 , 0];
 
         % CORRECTED: Use commas for a row vector output
-        f = @(t, y, p) [ p.p * (y(2) - y(1) - chua_diode(y(1), p.m0, p.m1)), ...
-                         p.q * (y(1) - y(2) + y(3)), ...
-                        -p.r * y(2) ];
+        f = @(t, y, p) [ p.alpha * (y(2) - y(1) - h(y(1), p.m0, p.m1)), ...
+                         y(1) - y(2) + y(3), ...
+                        -p.beta * y(2) ];
         
         plot_title = 'Fractional-Order Chua System';
 
-    case 'Bhalekar-Geiji'
-        p.omega = -2.667; p.miu = 10; p.a = 27.3, p.b = 1;
+    case 'Bhalekar-Gejji'
+        p.omega = -2.667; p.miu = 10; p.a = 27.3; p.b = 1;
         y0 = [0.7, 0.7, 0.7];
 
         % CORRECTED: Use commas for a row vector output
